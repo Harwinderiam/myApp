4e92290078dbe205d3502ab286c702e3a11b5490
harwinderiam <harwindercode@gmail.com>
2023-10-09 10:39:14 +0530

    Created Form

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require('http')
+const express = require('express')
+const app = express()
 
-http.createServer( (request, response) => {
+app.get("/", (req,res) => {
+    res.sendFile("form.html", {root : "."})
+})
+
+http.createServer(app).listen(4000)
+
+/*http.createServer( (request, response) => {
     console.log("Hello World")
     response.write("Hello World")
     response.end()
-}).listen(4000)
+}).listen(4000)*/
